6d8eaaed6830746c26edfec2e7efe873f6fdb162
Irfan Yilmaz <iy.usa@outlook.com>
2023-12-13 15:55:29 -0500

    implement API call with user input, display result in div
    use text input box to get city from user
    make API call when button clicked using inputted city
    await response and then pass json formatted version to local function
    let local function create and return HTML to display the recieved info
    allow resultDiv HTML to mirror newly returned HTML from local function

--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -1,17 +1,59 @@
+/* eslint-disable prefer-destructuring */
 
 
+const API_KEY = "API_KEY";
 const resultDiv = document.getElementById("resultsDiv");
 
 
-export default async function makeApiCall(name) {
+const weatherDisplay = (response) => {
 
-    const url = `https://api.agify.io?name=${name}`;
+    let city = response.location.name;
+    const country = response.location.country;
+    const description = response.current.condition.text;
+    const farenheit = response.current.temp_f;
+    const celsius = response.current.temp_c;
+
+    if(city !== response.location.region){
+    // ex. 'Orlando' becomes 'Orlando, Florida' but 'Tokyo' does not become 'Tokyo, Tokyo'
+            city += `, ${response.location.region}`
+    };
+
+    function makeWeatherDiv(){
+        const parentDiv = document.createElement("div");
+
+        const locationDiv = document.createElement("div");
+        locationDiv.innerText = `${city}, ${country}`;
+
+        const numbersDiv = document.createElement("div");
+        numbersDiv.innerText = `Farenheit: ${farenheit}, Celsius: ${celsius}`;
+        
+        const descriptionDiv = document.createElement("div");
+        descriptionDiv.innerText = `${description}`;
+        
+        parentDiv.appendChild(locationDiv);
+        parentDiv.appendChild(numbersDiv);
+        parentDiv.appendChild(descriptionDiv);
+
+        return parentDiv;
+    };
+
+    return makeWeatherDiv();
+};
+
+
+export default async function makeApiCall(city) {
+
+    const url = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}`;
     try {
         const res = await fetch(url);
         const response = await res.json();
-        resultDiv.innerHTML = response.age;
+
+        const forecast = weatherDisplay(response);
+        resultDiv.innerHTML = forecast.innerHTML;
+
     } catch(error) {
-        console.log(error) 
+        resultDiv.innerText = "Error Processing Your Request";
+        console.log(error);
     }
 
 };
